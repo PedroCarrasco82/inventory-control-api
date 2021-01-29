6618609c0f0ae7cc77b2b7421bd1c646990d00f5
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2021-01-29 20:27:41 -0300

    test: Ensure cnpjValidation wil return false when provided invalid CNPJ and true for correct ✅

--- __tests__/unit/utils.test.js
+++ __tests__/unit/utils.test.js
@@ -1,4 +1,4 @@
-const {emailValidation, cpfValidation} = require('../../src/utils/index')
+const {emailValidation, cpfValidation, cnpjValidation} = require('../../src/utils/index')
 
 describe('Utils', () => {
     it('Ensure emailValidation function will return false when provided email is wrong', () => {
@@ -9,11 +9,19 @@ describe('Utils', () => {
         expect(emailValidation('pedrinho123456@gatinhos.com')).toBe(true)
     })
 
-    it('Ensure cpfCnpjValidation function will return false when provided cpf is wrong', () => {
+    it('Ensure cpfValidation function will return false when provided cpf is wrong', () => {
         expect(cpfValidation('743916540')).toBe(false)
     })
 
-    it('Ensure cpfCnpjValidation function will return true when provided cpf is correct', () => {
+    it('Ensure cpfValidation function will return true when provided cpf is correct', () => {
         expect(cpfValidation('45360285877')).toBe(true)
+    })
+
+    it('Ensure cnpjValidation function will return false when provided cnpj is wrong', () => {
+        expect(cnpjValidation('67.548.121/0003-43')).toBe(false)
+    })
+
+    it('Ensure cnpjValidation function will return true when provided cnpj is correct', () => {
+        expect(cnpjValidation('67.548.121/0001-43')).toBe(true)
     })
 })
