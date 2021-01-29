d83d4e6f5ff433e2636e78458932d31b0e004027
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2021-01-29 18:36:10 -0300

    test: Ensure cpfValidaiton return false for wrong cpf and true for correct ✅

--- __tests__/unit/utils.test.js
+++ __tests__/unit/utils.test.js
@@ -1,4 +1,4 @@
-const {emailValidation} = require('../../src/utils/index')
+const {emailValidation, cpfValidation} = require('../../src/utils/index')
 
 describe('Utils', () => {
     it('Ensure emailValidation function will return false when provided email is wrong', () => {
@@ -7,5 +7,13 @@ describe('Utils', () => {
 
     it('Ensure emailValidation function will return true when provided email is correct', () => {
         expect(emailValidation('pedrinho123456@gatinhos.com')).toBe(true)
+    })
+
+    it('Ensure cpfCnpjValidation function will return false when provided cpf is wrong', () => {
+        expect(cpfValidation('743916540')).toBe(false)
+    })
+
+    it('Ensure cpfCnpjValidation function will return true when provided cpf is correct', () => {
+        expect(cpfValidation('45360285877')).toBe(true)
     })
 })
