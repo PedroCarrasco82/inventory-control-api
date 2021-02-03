795a1bac12c5a326f93c911b516e4953cb097526
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2021-02-03 14:33:31 -0300

    test: Ensure will return status 400 when provided invalid CEP and true for valid ✅

--- __tests__/integration/users.test.js
+++ __tests__/integration/users.test.js
@@ -6,7 +6,9 @@ const app = require('../../src/app')
 const createUserMock = {
     email: 'pedrinho123456@gatinhos.com',
     cpfCnpj: '526.597.020-71',
-    telephoneNumber: '(31)9923-23288'
+    telephoneNumber: '(31)91923-2328',
+    companyCEP: '59070-410',
+    userCEP: '59122-042'
 }
 
 describe('Users route', () => {
@@ -30,7 +32,7 @@ describe('Users route', () => {
         const body = { ...createUserMock }
         body.cpfCnpj = '08081343010108'
         const response = await request(app).post('/users/signup').send(body)
-
+        
         expect(response.status).toBe(400)
     })
 
@@ -39,6 +41,22 @@ describe('Users route', () => {
         body.telephoneNumber = '213as444'
         const response = await request(app).post('/users/signup').send(body)
 
+        expect(response.status).toBe(400)
+    })
+
+    it('Ensure return status 400 if invalid companyCEP is provided', async () => {
+        const body = { ...createUserMock }
+        body.companyCEP = '7329405754984378743'
+        const response = await request(app).post('/users/signup').send(body)
+
+        expect(response.status).toBe(400)
+    })
+
+    it('Ensure return status 400 if invalid userCEP is provided', async () => {
+        const body = { ...createUserMock }
+        body.userCEP = '654456456576547'
+        const response = await request(app).post('/users/signup').send(body)
+        
         expect(response.status).toBe(400)
     })
 })
