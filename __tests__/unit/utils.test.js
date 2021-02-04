b17897f8788694cda9e1b725bb5c78e6863df0a9
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2021-02-04 18:33:04 -0300

    test: Ensure validateBirthdayDate function will return false when provided invalid birthday date and true for valid ✅

--- __tests__/unit/utils.test.js
+++ __tests__/unit/utils.test.js
@@ -1,42 +1,52 @@
 'use strict'
 
-const {
-    emailValidation, 
-    cpfValidation, 
-    cnpjValidation,
-    CEPValidation
-} = require('../../src/utils/index')
+const { 
+    validateEmail, 
+    validateCPF,
+    validateCNPJ, 
+    validateTelephoneNumber,
+    validateCEP,
+    validateBirthdayDate
+} = require('../../src/utils/validations')
 
 describe('Utils', () => {
     it('Ensure emailValidation function will return false when provided email is wrong', () => {
-        expect(emailValidation('pedrinho123456.com')).toBe(false)
+        expect(validateEmail('pedrinho123456.com')).toBe(false)
     })
 
     it('Ensure emailValidation function will return true when provided email is correct', () => {
-        expect(emailValidation('pedrinho123456@gatinhos.com')).toBe(true)
+        expect(validateEmail('pedrinho123456@gatinhos.com')).toBe(true)
     })
 
     it('Ensure cpfValidation function will return false when provided cpf is wrong', () => {
-        expect(cpfValidation('743916540')).toBe(false)
+        expect(validateCPF('743916540')).toBe(false)
     })
 
     it('Ensure cpfValidation function will return true when provided cpf is correct', () => {
-        expect(cpfValidation('45360285877')).toBe(true)
+        expect(validateCPF('45360285877')).toBe(true)
     })
 
     it('Ensure cnpjValidation function will return false when provided cnpj is wrong', () => {
-        expect(cnpjValidation('67.548.121/0003-43')).toBe(false)
+        expect(validateCNPJ('67.548.121/0003-43')).toBe(false)
     })
 
     it('Ensure cnpjValidation function will return true when provided cnpj is correct', () => {
-        expect(cnpjValidation('67.548.121/0001-43')).toBe(true)
+        expect(validateCNPJ('67.548.121/0001-43')).toBe(true)
     })
 
     it('Ensure CEPValidation function will return false when provided CEP is wrong', () => {
-        expect(CEPValidation('2743694553456345')).toBe(true)
+        expect(validateCEP('2743694553456345')).toBe(false)
     })
 
     it('Ensure CEPValidation function will return true when provided CEP is correct', () => {
-        expect(CEPValidation('79084-081')).toBe(true)
+        expect(validateCEP('79084-081')).toBe(true)
+    })
+
+    it('Ensure validateBirthdayDate function will return false when provided birthday date is wrong', () => {
+        expect(validateBirthdayDate('20010-30321-12321')).toBe(false)
+    })
+    
+    it('Ensure validateBirthdayDate function will return true when provided birthday date is correct', () => {
+        expect(validateBirthdayDate('2001-01-12')).toBe(true)
     })
 })
